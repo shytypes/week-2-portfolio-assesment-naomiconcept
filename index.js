a3b66e6c8b8edc50085dcfef259e7d72bff2ef2e
Aj <conceptnaomi@gmail.com>
2025-05-15 14:09:05 +0100

    Updated

---
+++ index.js
@@ -0,0 +1,18 @@
+document.getElementById('myForm').addEventListener('submit', function (event) {
+  event.preventDefault()
+
+  const fname = document.getElementById('fname').value
+  const sname = document.getElementById('sname').value
+  const email = document.getElementById('email').value
+  const message = document.getElementById('message').value
+
+  const formDetails = document.getElementById('formDetails')
+  formDetails.innerHTML = ` <h3 style = "color: white;">Thank you for reaching out😃</h3>
+                            <p><strong>First Name: </strong> ${fname}</p>
+                            <p><strong>Second Name: </strong> ${sname}</p>
+                            <p><strong>Email: </strong> ${email}</p>
+                            <p><strong>Message </strong> ${message}</p>`
+  formDetails.style.display = 'block'
+
+  document.querySelector('.form-container').style.display = 'none'
+})
